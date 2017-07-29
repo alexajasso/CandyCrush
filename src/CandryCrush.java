0cf1d26e49187fed068cfd6eebeff263fcb51472
League <Level 2>
2017-07-29 11:32:00 -0700

    I had fun.

---
+++ src/CandryCrush.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+
+public class CandryCrush {
+	public static void main(String[] args) {
+		String name = JOptionPane.showInputDialog("who do you like?");
+		JOptionPane.showMessageDialog(null, " you totally have a crush on " + name);
+
+	}
+}
